40e44f6fa51aa428079eab57baef09fe1f3cb1e2
OpenShift guest <tcmorin@gmail.com>
2013-06-11 06:45:26 -0400

    add jslint grunt plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
 
+    'use strict';
+
     grunt.initConfig({
         connect: {
             server: {
@@ -8,9 +10,54 @@ module.exports = function (grunt) {
                     base: './'
                 }
             }
+        },
+
+        jslint: { // configure the task
+            files: [ // some example files
+                'Gruntfile.js',
+                'wc.js'
+            ],
+            directives: {
+                ass: false,
+                bitwise: false,
+                browser: true,
+                closure: false,
+                'continue': false,
+                couch: false,
+                debug: false,
+                devel: true,
+                eqeq: false,
+                es5: true,
+                evil: false,
+                forin: false,
+                indent: 4,
+                maxerr: 100,
+                maxlen: 100,
+                newcap: false,
+                node: true,
+                nomen: false,
+                passfail: false,
+                plusplus: false,
+                regexp: true,
+                rhino: false,
+                sloppy: false,
+                stupid: false,
+                sub: false,
+                todo: true,
+                unparam: false,
+                vars: false,
+                white: false,
+
+                predef: ['require', 'define']
+            },
+            options: {
+                failOnError: false, // defaults to true
+                shebang: true // ignore shebang lines
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-jslint');
 
 };
